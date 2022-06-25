4032f8dabdba656c61a2809f72ecdf2c0df178bb
ManjunathPrasad <manjunathprasad99@gmail.com>
2022-06-25 06:51:35 +0530

    Serialization And Desiralization Demo

---
+++ SerializeDeserialize/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserialize
+{
+    [Serializable]
+    class Library
+    {
+        public int _isbn { get; set; }
+
+        public string _bookTitle { get; set; }
+
+        public String _author { get; set; }
+
+        public Library(int isbn, string bookTitle, string author)
+        {
+            this._isbn = isbn;
+
+            this._bookTitle = bookTitle;
+
+            this._author = author;
+        }
+    }
+}
---
+++ SerializeDeserialize/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDeserialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Serialization of object to stream
+            string path = @"C:\Users\v-mholen\source\repos\SerializeDeserialize\Output.txt";
+
+            Library library = new Library(5437821, "The God Father", "Mario Puzo");
+
+            FileStream filestream = new FileStream(path, FileMode.OpenOrCreate);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(filestream, library);
+
+            filestream.Close();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("File saved in the path" + path);
+
+            Console.ReadKey();
+
+            //Desrialization of stream  to object
+
+            FileStream filestream1 = new FileStream(path, FileMode.OpenOrCreate);
+
+            BinaryFormatter formatter1 = new BinaryFormatter();
+
+            Library library1 = (Library)formatter1.Deserialize(filestream1);
+
+            Console.WriteLine("Book ISBN : " + library1._isbn);
+
+            Console.WriteLine("Book Title : " + library1._bookTitle);
+
+            Console.WriteLine("Book Author : " + library1._author);
+
+            Console.ReadKey();
+        }
+    }
+}
